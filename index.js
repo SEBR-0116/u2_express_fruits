6b2b3550db74edd8839b3d7a0c8392b37731cc59
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-02-15 07:26:31 -0500

    Finished fruits hw

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const express = require('express')
+const PORT = process.env.PORT || 3001;
+const app = express()
 
+app.get('/', (req, res) => {
+  console.log('initial landing page')
+  res.send('initial landing page')
+
+})
+app.get('/hello', (req, res) => {
+  console.log('hello world!')
+  res.send('hello world!')
+
+})
+app.get('/greet/:name', (request, response) => {
+  console.log(`Hello${request.params.name}`)
+  response.send({ msg: `Hello ${request.params.name}` })
+})
 app.get('/five', (req, res) => {
     let fiveArray = [1,2,3,4,5];
     res.send(`${fiveArray}`)
@@ -11,6 +28,12 @@ app.get('/fruits', (req, res) => {
 
   app.get('/fruits/:name', (req, res) => {
     let fruits = ['apple', 'orange', 'kiwi']
-    name
-    res.send(`${fruits}`)
+    res.send(fruits[req.params.name])
   })
+
+  app.get('/veggies/:name', (req, res) => {
+    let veggies = ['carrot', 'lettuce', 'kale']
+    res.send(veggies[req.params.id])
+  })
+
+  app.listen(PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`))
